1e741680eb5fe96978f9d86b8f7495c65016c04a
prave <prave@172.10.13.75>
2021-04-07 14:23:15 +0530

    collections

---
+++ Project1/src/collection/BooksPriorityQue.java
@@ -0,0 +1,11 @@
+package collection;
+
+import java.util.Queue;
+public class BooksPriorityQue {
+	public static void main(String[] args) {
+		Queue< Book> books = BooksData.getBookData();
+		for(Book b: books) {
+			System.out.println(b.id+" "+b.bookName+" "+b.author+" "+b.publisher+" "+b.quantity);  
+		}
+	}	
+}
---
+++ Project1/src/collection/HashMapEx.java
@@ -0,0 +1,10 @@
+package collection;
+
+import java.util.HashMap;
+public class HashMapEx {
+	public static void main(String[] args) {
+		HashMap<String, Integer> scoresHashMap = new HashMap<String, Integer>();
+		scoresHashMap.put("KING", 16);
+		scoresHashMap.put("QUEEN", 13); 
+	}
+}
