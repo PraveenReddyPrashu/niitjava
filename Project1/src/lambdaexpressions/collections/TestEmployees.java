cbed7a409089e251e7b374567f01f6772346ed65
prave <prave@DESKTOP-PO73MRJ>
2021-04-08 14:46:19 +0530

    Lambda Expressions

--- Project1/src/lambdaexpressions/collections/TestEmployees.java
+++ Project1/src/lambdaexpressions/collections/TestEmployees.java
@@ -8,16 +8,18 @@ public class TestEmployees {
 
 	public static void main(String[] args) {
 		ArrayList<Employee> employees = new ArrayList<Employee>();
-		employees.add(new Employee("Praveen",34));
-		employees.add(new Employee("Venkata",56));
-		employees.add(new Employee("Vinay",45));
+		employees.add(new Employee("Praveen",56));
+		employees.add(new Employee("Kiran",45));
+		employees.add(new Employee("Vinay",34));
 		employees.add(new Employee("Pavan",23));
 		employees.add(new Employee("Mohan",12));
 
 		System.out.println(employees);
 		Comparator<Employee> c = (e1,e2) -> (e1.eid < e2.eid)? -1 :(e1.eid > e2.eid)? 1 :0;
-
+		Comparator<Employee> n = (e1,e2) -> e1.name.compareTo(e2.name);
 		Collections.sort(employees,c);
+		System.out.println(employees);
+		Collections.sort(employees,n);
 		System.out.println(employees);
 
 	}
